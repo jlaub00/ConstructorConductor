762d41bf314716dc65510e48a5b754261733aec2
jlaub00 <jlaub00@yahoo.com>
2015-10-07 20:30:17 -0600

    2nd Save

--- quizApp.js
+++ quizApp.js
@@ -6,27 +6,39 @@
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
+var User = function(name, email, password, totalScore){
+  this.name = name;
+  this.email = email;
+  this.password = password;
+  this.totalScore;
+}
+  
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
-
+var Question = function(answerArray, rightAnswer, difficulty){
+  this.answerArray = answerArray;
+  this.rightAnswer = rightAnswer;
+  this.difficulty = difficulty;
+}
 
 //Create a users Array which is going to hold all of our users.
 
   //code here
-
+var usersArray = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
-
-
+var bob = new User("Bob", "bob@bob.com", "bob1", 20 );
+var bill = new User("Bill", "bill@bill.com", "bill1", 21);
+var jill = new User("Jill", "jill@jill.com", "jill1", 25);
 //Create a questions Array which is going to hold all of our questions
 
   //code here
-
+var questionsArray = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -35,13 +47,16 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
-
+var q1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?');
+var q2 = new Question('T/F: JavaScript is just a scripting version of Java');
+var q3 = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value");
 
 //Now push all of your instances of Question into the questions Array
 
   //code here
-
+Question.push(q1, q2, q3);{
 console.log('My users Array and my questions arrray are ...');
+}
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
   //code here
